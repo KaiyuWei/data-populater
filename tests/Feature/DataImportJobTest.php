ea7a94924fe5f3fc35cfbfe46e153182fc9dc0da
KaiyuWei <weikaiyu2020@outlook.com>
2023-11-03 10:56:19 +0100

    docker compose configured, now we can do tests in docker environment

--- tests/Feature/DataImportJobTest.php
+++ tests/Feature/DataImportJobTest.php
@@ -24,7 +24,7 @@ class DataImportJobTest extends TestCase
     public function test_job_write_data_to_database(): void
     {
         // the json file we use for testing
-        $filePath = "/Users/kaiyuwei/Downloads/challenge_1610.json";
+        $filePath = "/usr/local/bin/challenge_1610.json";
 
         // call the data import function, provide the file, the format of the file and the data fileter.
 
